bbab21aca63b6746b7166e5ba161c1dca75661c4
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-05-15 13:56:23 -0400

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import react from "react";
+
+function EyesOnMe() {
+    // const onFocus = focus 
+    // const onBlur = blur 
+    function handleFocus(){
+        return console.log('Good!')
+    }
+    function handleBlur(){
+        return console.log('Hey! Eyes on me!')
+    }
+    return(
+        <>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </>
+    )
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+import react from "react";
+
+
+function Keypad() {
+    return <input type="password" onChange={() => console.log('Entering password...')} />
+}
+
+export default Keypad;
